9bc325a496563f38d1ca65ab21ee9c21f250d44d
codehrdndtony <48218798+codehrdndtony@users.noreply.github.com>
2021-12-14 01:43:47 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,47 @@
 $(document).ready(function() {
   var currentFloor = 2;
+  var currentFlatNum = 1;
   let usCurrentFloor
   var floorPath = $(".home-image path");
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
+  var modal = $(".modal");
+  var modalFloorCounter = $(".modal-counter")
+  var modalCloseButton = $(".modal-close-button");
+  var flatsPath = $(".modal-image path");
+  var flatsLink = $(".flat-item a");
+  var viewFlatsButton = $(".view-flats");
 
   floorPath.on('mouseover', function () {
     floorPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-floor");
     $(".counter").text(currentFloor);
+  });
+
+  floorPath.on('click', toggleModal);
+  modalCloseButton.on('click', toggleModal);
+  viewFlatsButton.on('click', toggleModal);
+
+  // Flat plan hovering
+  flatsPath.on('mouseover', function () {
+    flatsLink.removeClass("current-link");
+    currentFlatNum = $(this).attr("data-flat-num");
+    $(`[data-flat-num=${currentFlatNum}]`).toggleClass("current-link");
+  });
+
+  flatsPath.on('mouseleave', function() {
+    flatsLink.removeClass("current-link")
+  });
+
+  // Flat link hovering
+  flatsLink.on('mouseover', function () {
+    flatsPath.removeClass("current-flat");
+    currentFlatNum = $(this).attr("data-flat-num");
+    $(`[data-flat-num=${currentFlatNum}]`).toggleClass("current-flat");
+  });
+
+  flatsLink.on('mouseleave', function() {
+    flatsPath.removeClass("current-flat")
   });
 
   counterUp.on('click', function () {
@@ -38,4 +71,15 @@ $(document).ready(function() {
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
     }
   })
+
+  function toggleModal() {
+    modalFloorCounter.text(currentFloor)
+    modal.toggleClass('is-open');
+  }
+
+  // function highlightOpposite(obj, attr, tglCls) {
+  //   obj.removeClass(tglCls);
+  //   currentFlatNum = $(this).attr(attr);
+  //   $(`[${attr}=${currentFlatNum}]`).toggleClass(tglCls);
+  // };
 });
